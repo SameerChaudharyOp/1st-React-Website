e9254a92c1d167aff965f2013dc7676944f52bf9
SameerChaudharyOp <sameerchaudhary749@gmail.com>
2022-08-23 15:16:39 +0530

    Resturant Website

---
+++ src/ProductData.js
@@ -0,0 +1,23 @@
+const ProductData = {
+      title: 'FitBit 19 - The Smartest Watch',
+      description: 'Smart Watch With 24 Hour battery Playback Water Resistant Avaliable In 4 Different Color',
+      ColorOptions: [
+            {
+                styleName: 'Black Strap',
+                imageUrl: 'https://imgur.com/iOeUB7.png'
+            },
+            {
+                styleName: 'Red Strap',
+                imageUrl: 'https://imgur.com/PTgQlim.png'
+            },
+            {
+                styleName: 'Blue Strap',
+                imageUrl: 'https://imgur.com/Mp1j1YR.png'
+            },
+            {
+                styleName: 'Purple Strap',
+                imageUrl: 'https://imgur.com/xSIK4M8.png'
+            }
+      ]
+
+}
